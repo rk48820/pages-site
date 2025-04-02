a466df823284f33dff4f7a424ef2c0cc77085ab1
Robert <rk48820@email.vccs.edu>
2025-04-01 20:19:10 -0400

    adding new links to vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import _ from "lodash";
 
 export default defineConfig({
   base: "/pages-site/",  
@@ -12,7 +13,11 @@ export default defineConfig({
         timer: resolve(__dirname, "timer/index.html"),
         fcccolorchanger: resolve(__dirname, "fcccolorchanger/index.html"),
         palindromechecker: resolve(__dirname, "palindromechecker/index.html"),
-        romannumeralconverter: resolve(__dirname, "romannumeralconverter/index.html")
+        romannumeralconverter: resolve(__dirname, "romannumeralconverter/index.html"),
+        telephonevalidator: resolve(__dirname, "telephonevalidator/index.html"),
+        cashregisterproject: resolve(__dirname, "cashregisterproject/index.html"), 
+        pokemonsearchapp: resolve(__dirname, "pokemonsearchapp/index.html"), 
+        recipeorganizerandmealplanner: resolve (__dirname, "recipeorganizerandmealplanner/index.html")
 
       },
     },
